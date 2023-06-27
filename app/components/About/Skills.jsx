c15e08a7d6c346cfc77ea7bd746e95dda6971e03
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-06-27 19:10:47 +0300

    skills

--- app/components/About/Skills.jsx
+++ app/components/About/Skills.jsx
@@ -1,10 +1,31 @@
 "use client";
-import { AnimatedTextCharacter } from "@/app/animations";
+import { AnimatedTextCharacter, FadeInDiv } from "@/app/animations";
+import { skills } from "@/app/constants/Skills";
+import Image from "next/image";
+import Tilt from "react-parallax-tilt";
+
+const Skill = ({ key, name, icon }) => {
+  return (
+    <div className="skill">
+      <Image src={icon} width={80} alt={key} />
+      <h3>{name}</h3>
+    </div>
+  );
+};
 
 const Skills = () => {
   return (
     <div className="skills">
       <AnimatedTextCharacter className="margin med" text="Skills" />
+      <FadeInDiv className="skills_con">
+        {skills.map((skill) => {
+          return (
+            <Tilt key={skill.name} className="skill_con">
+              <Skill name={skill.name} icon={skill.icon} />
+            </Tilt>
+          );
+        })}
+      </FadeInDiv>
     </div>
   );
 };
---
+++ app/constants/Skills.js
@@ -0,0 +1,96 @@
+import javascript from "@/public/images/skills_icons/javascript.png";
+import html from "@/public/images/skills_icons/html.png";
+import css from "@/public/images/skills_icons/css.png";
+import reactjs from "@/public/images/skills_icons/reactjs.png";
+import redux from "@/public/images/skills_icons/redux.png";
+import tailwind from "@/public/images/skills_icons/tailwind.png";
+import threejs from "@/public/images/skills_icons/threejs.svg";
+import git from "@/public/images/skills_icons/git.png";
+import nextjs from "@/public/images/skills_icons/nextjs.png";
+import npm from "@/public/images/skills_icons/npm.png";
+import opencart from "@/public/images/skills_icons/opencart.png";
+import responsive from "@/public/images/skills_icons/responsive.png";
+import sass from "@/public/images/skills_icons/sass.png";
+import styled from "@/public/images/skills_icons/styled.png";
+import vite from "@/public/images/skills_icons/vite.svg";
+import wordpress from "@/public/images/skills_icons/wordpress.png";
+import framer from "@/public/images/skills_icons/framer.png";
+import figma from "@/public/images/skills_icons/figma.png";
+
+export const skills = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Scss",
+    icon: sass,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+
+  {
+    name: "Npm",
+    icon: npm,
+  },
+  {
+    name: "Next Js",
+    icon: nextjs,
+  },
+  {
+    name: "Vite",
+    icon: vite,
+  },
+  {
+    name: "Opencart",
+    icon: opencart,
+  },
+
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+  {
+    name: "Wordpress",
+    icon: wordpress,
+  },
+  {
+    name: "Framer Motion",
+    icon: framer,
+  },
+  {
+    name: "Styled Components",
+    icon: styled,
+  },
+  {
+    name: "Responsive Web Design",
+    icon: responsive,
+  },
+];
