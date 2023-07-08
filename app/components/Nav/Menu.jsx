deddb188299291adb69d95dc01039687e2a520c6
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-07-08 11:22:39 +0300

    nav scroll top

--- app/components/Nav/Menu.jsx
+++ app/components/Nav/Menu.jsx
@@ -6,21 +6,31 @@ const Menu = () => {
   const path = usePathname();
   return (
     <nav className="menu">
-      <Link className={`${path === "/" ? "active" : ""}`} href="/">
+      <Link
+        className={`${path === "/" ? "active" : ""}`}
+        href="/"
+        scroll={true}
+      >
         Home
       </Link>
-      <Link className={`${path === "/about" ? "active" : ""}`} href="/about">
+      <Link
+        className={`${path === "/about" ? "active" : ""}`}
+        href="/about"
+        scroll={true}
+      >
         About
       </Link>
       <Link
         className={`${path === "/projects" ? "active" : ""}`}
         href="/projects"
+        scroll={true}
       >
         Projects
       </Link>
       <Link
         className={`${path === "/contact" ? "active" : ""}`}
         href="/contact"
+        scroll={true}
       >
         Contact
       </Link>
