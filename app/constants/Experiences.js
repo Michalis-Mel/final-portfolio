27e9c2932617003283bb4c6aceb1a2d1c4518fb9
Michael Meliopoulos <mixalismeliop@gmail.com>
2023-12-06 14:55:43 +0200

    added progressive robotics

--- app/constants/Experiences.js
+++ app/constants/Experiences.js
@@ -1,23 +1,36 @@
-import mandala from "@/public/images/experience_icons/mandala.png";
 import webnerds from "@/public/images/experience_icons/webnerds.png";
-import prestige from "@/public/images/experience_icons/prestige.png";
 import gold from "@/public/images/experience_icons/gold.png";
-import celso from "@/public/images/experience_icons/celso.png";
+import progressive from "@/public/images/experience_icons/progressive-robotics-logo.png";
 
 export const experiences = [
   {
-    title: "Front End Web Developer",
+    title: "Front End Developer",
     company_name: "Webnerds",
     icon: webnerds,
     iconBg: "#fff",
     date: "March 2021 - March 2023",
     points: [
-      "Developing and maintaining web applications and e-shops using HTML, CSS, JQuery & Javascript, Wordpress, Opencart and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed and maintained web applications and e-shops using HTML, CSS, jQuery, JavaScript, WordPress, Opencart, and other related technologies at Webnerds.",
+      "Collaborated effectively with cross-functional teams, including designers, product managers, and fellow developers, to contribute to the creation of high-quality digital products.",
+      "Implemented responsive design techniques to ensure optimal user experiences and ensured cross-browser compatibility for seamless accessibility.",
+      "Actively participated in code reviews, providing constructive feedback to enhance code quality and foster a collaborative development environment at Webnerds.",
     ],
     link: "https://www.webnerds.gr",
+  },
+  {
+    title: "Front End Developer",
+    company_name: "Progressive Robotics",
+    icon: progressive,
+    iconBg: "#fff",
+    date: "March 2021 - March 2023",
+    points: [
+      "As the sole Front-End Developer at Progressive Robotics, I am at the helm of architecting and managing the entire front-end ecosystem for our cutting-edge application.",
+      "Leveraging technologies such as React, TypeScript, Three.js, and Mobx-State-Tree, I contribute to the innovative forefront of the robotics industry through the development and optimization of our state-of-the-art application.",
+      "Beyond coding, I showcase my creativity as a web designer, shaping captivating layouts, logos, and images in Figma to elevate the visual and interactive aspects of our application.",
+      "Serving as the linchpin between frontend and backend, I facilitate daily communication with our brilliant backend developers, ensuring seamless collaboration on API development, database management, and critical algorithms vital for the success of our robotic arms.",
+      "In our daily endeavors, we embrace the precision of Git for our  source code management, ensuring the integrity, collaboration, and version control of our application's codebase.",
+    ],
+    link: "https://progressiverobotics.ai/",
   },
   {
     title: "Bartender & Barista",
